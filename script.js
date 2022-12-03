fc398b78c98e167acd12f5c5b672b9df461d6042
faiq28 <faiqafifalthf@gmail.com>
2022-12-03 09:22:08 +0700

    first commit

--- script.js
+++ script.js
@@ -6,32 +6,32 @@
 //         nav.classList.remove("sticky");
 //     }
 // })
-<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+{/* <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script> */}
 
-var swiper = new Swiper(".mySwiper", {
-    spaceBetween: 30,
-    centeredSlides: true,
-    autoplay: {
-      delay: 2500,
-      disableOnInteraction: false,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
-  let circle = document.querySelector(".color-option");
+// var swiper = new Swiper(".mySwiper", {
+//     spaceBetween: 30,
+//     centeredSlides: true,
+//     autoplay: {
+//       delay: 2500,
+//       disableOnInteraction: false,
+//     },
+//     pagination: {
+//       el: ".swiper-pagination",
+//       clickable: true,
+//     },
+//     navigation: {
+//       nextEl: ".swiper-button-next",
+//       prevEl: ".swiper-button-prev",
+//     },
+//   });
+//   let circle = document.querySelector(".color-option");
 
-  circle.addEventListener("click", (e)=>{
-    let target = e.target;
-    if(target.classList.contains("circle")){
-      circle.querySelector(".active").classList.remove("active");
-      target.classList.add("active");
-      document.querySelector(".main-images .active").classList.remove("active");
-      document.querySelector(`.main-images .${target.id}`).classList.add("active");
-    }
-  });
+//   circle.addEventListener("click", (e)=>{
+//     let target = e.target;
+//     if(target.classList.contains("circle")){
+//       circle.querySelector(".active").classList.remove("active");
+//       target.classList.add("active");
+//       document.querySelector(".main-images .active").classList.remove("active");
+//       document.querySelector(`.main-images .${target.id}`).classList.add("active");
+//     }
+//   });
